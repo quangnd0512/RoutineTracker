e6d0e87c277cd7a0ebe24e9cc9469f36353d4e1d
moonsmile-dev <kakavip.198.kaka@gmail.com>
2025-08-11 12:40:36 +0700

    feat: init gluestack ui

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,6 @@
 import "@/global.css";
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
+// import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
@@ -19,12 +20,14 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <GluestackUIProvider mode="light">
+      {/* <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}> */}
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="+not-found" />
+        </Stack>
+        <StatusBar style="auto" />
+      {/* </ThemeProvider> */}
+    </GluestackUIProvider>
   );
 }
