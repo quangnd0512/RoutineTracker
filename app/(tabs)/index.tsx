eccd42179fa1047adbb66a52c3cbc8fe2297d59e
moonsmile-dev <kakavip.198.kaka@gmail.com>
2025-08-27 11:05:12 +0700

    feat: update data for weekly activity

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,9 @@
 import CircleCheckIcon from "@/components/icons/CircleCheckIcon";
-import { Dimensions, View, Text } from "react-native";
+import log from "@/services/logger";
+import { RoutineTaskService } from "@/services/routineTaskService";
+import { useFocusEffect } from "expo-router";
+import { useCallback, useEffect, useRef, useState } from "react";
+import { Dimensions, Text, View } from "react-native";
 import { Calendar } from "react-native-calendars";
 import { LineChart } from "react-native-chart-kit";
 
@@ -18,6 +22,7 @@ const MonthlyCalendar = () => {
         <Text className="font-bold">Monthly Activity</Text>
       </View>
       <Calendar
+        className="rounded-lg"
         markedDates={markedDates}
         dayComponent={({ date, marking, state }) => {
           const isMarked = marking?.marked;
@@ -43,19 +48,51 @@ const MonthlyCalendar = () => {
 };
 
 const WeeklyChart = () => {
+  const [taskCounts, setTaskCounts] = useState<number[]>([0, 0, 0, 0, 0, 0, 0]);
+  
+  async function fetchTasks() {
+    const _now = new Date();
+    let weekDay = _now.getDay(); // 0 (Sun) to 6 (Sat)
+    if (weekDay === 0) weekDay = 7; // Make Sunday the last day
+
+    const tasks = [];
+
+    for (let i = 1; i <= weekDay; i++) {
+      const atDate = new Date(_now);
+      atDate.setDate(_now.getDate() - weekDay + i);
+
+      tasks.push(
+        RoutineTaskService.getFinishedRoutineTasks(atDate)
+      );
+    }
+
+    const results = await Promise.all(tasks);
+    const counts = results.map(res => res.length);
+
+    while (counts.length < 7) {
+      counts.push(0);
+    }
+
+    log.info(`[HomeScreen] Weekly task counts: ${counts}`);
+    setTaskCounts(counts);
+  }
+
+  const memoizedFetchTasks = useCallback(() => {
+    fetchTasks();
+
+    return () => {
+      setTaskCounts([0, 0, 0, 0, 0, 0, 0]);
+    };
+  }, []);
+
+  useFocusEffect(memoizedFetchTasks);
+
   // Sample data for the line chart
   const data = {
     labels: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
     datasets: [
       {
-        data: [
-          0,
-          7,
-          9,
-          10,
-          11,
-          1,
-        ],
+        data: taskCounts,
       },
     ],
   };
